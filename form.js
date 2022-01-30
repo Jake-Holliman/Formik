712b4227e6754b2933e44d950d45f35cfe66548c
Jake-Holliman <88987855+Jake-Holliman@users.noreply.github.com>
2022-01-30 10:21:15 -0600

    Update form.js

--- form.js
+++ form.js
@@ -1,21 +1,26 @@
-function TodoForm({addTodo}){
-    const [value, setValue] = React.useState('');
+function TodoForm({ addTask }) {
+	const [value, setValue] = React.useState("");
 
-    const handleSubmit = e =>{
-        e.preventDefault();
-        if (!value) return;
-        addTodo(value);
-        const newTodos = [...todos, {text:text, isCompleted:false}];
-        setTodos(newTodos);
-        setValue('');
-      };
-      
-    return (<form onSubmit={handleSubmit}>
-    <input type="text"
-            className="input"
-            value={value}
-            placeholder="add to-do"
-            onChange={e => setValue(e.target.value)} />
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!value) return;
+		addTask(value);
+		setValue("");
+	};
 
-  </form>);
+	return (
+		<form onSubmit={handleSubmit} className="form">
+			<input
+				value={value}
+				type="text"
+				className="input"
+				placeholder="I need to..."
+				onChange={(e) => setValue(e.target.value)}
+			/>
+			<button className="button" type="submit">
+				Add Task
+				<i class="add bi bi-journal-plus"></i>
+			</button>
+		</form>
+	);
 }
